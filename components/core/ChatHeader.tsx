3cb197dd1d0b3f06130e85567f5c7086935b9ce4
iamplus-mrunali <mrunali@iamplus.in>
2026-01-29 18:50:41 +0530

    added chnages in chatheader

--- components/core/ChatHeader.tsx
+++ components/core/ChatHeader.tsx
@@ -12,26 +12,23 @@ export function ChatHeader({ title, onClose }: ChatHeaderProps) {
     const insets = useSafeAreaInsets();
 
     return (
-        <View
-            className="flex-row items-center justify-between px-4 pb-3 bg-white border-b-[1px] border-border-subtle relative"
-            style={{ paddingTop: Math.max(insets.top, 20) }}
-        >
+        <View className="bg-white" style={{ paddingTop: Math.max(insets.top, 20) }}>
+            <View className="flex-row items-center justify-between px-4 pb-3">
+                <View className="w-[24px]" />
 
-            <View className="w-[24px]" />
+                <Text className="text-[18px] leading-[21px] font-semibold text-text-figma-gray text-center tracking-[-0.32px] font-inter">
+                    {title}
+                </Text>
 
-
-            <Text className="text-[18px] leading-[21px] font-semibold text-text-figma-gray text-center tracking-[-0.32px] font-inter">
-                {title}
-            </Text>
-
-
-            <TouchableOpacity
-                onPress={onClose}
-                activeOpacity={0.7}
-                className="w-[24px] items-end justify-center"
-            >
-                <CloseIcon width={14} height={14} color="var(--text-dark)" />
-            </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={onClose}
+                    activeOpacity={0.7}
+                    className="w-[24px] items-center justify-center"
+                >
+                    <CloseIcon width={14} height={14} color="var(--text-dark)" />
+                </TouchableOpacity>
+            </View>
+            <View className="h-[1px] bg-border-subtle mx-4 mb-2" />
         </View>
     );
 }
